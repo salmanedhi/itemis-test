cc97c6909e7fa342e8fd0a51e14d78b11460023b
Muhammad Salman Edhi <salman.edhi@gmail.com>
2022-02-13 13:25:11 +0100

    wrote test to check items list to not be empty
    - Test added in the TestApplicationTests.java to check that the items list containing the items of the receipt taken as input is not empty.
    - test failed because ItemDto does not exist

--- src/test/java/com/itemis/test/TestApplicationTests.java
+++ src/test/java/com/itemis/test/TestApplicationTests.java
@@ -4,13 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 
 @SpringBootTest
 class TestApplicationTests {
 
 	@Test
-	void contextLoads() {
-		assertEquals(true, true);
+	void checkItemListToNotBeEmpty(List<ItemDto> itemsList) {
+		
+		assertNotNull(itemsList);
+		assertFalse(itemsList.isEmpty());
 	}
 
 }
