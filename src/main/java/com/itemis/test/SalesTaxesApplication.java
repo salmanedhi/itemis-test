7a9437e227f574d857c1a202801ac2ac6046a329
Muhammad Salman Edhi <salman.edhi@gmail.com>
2022-02-13 13:08:35 +0100

    Initial project structure created

---
+++ src/main/java/com/itemis/test/SalesTaxesApplication.java
@@ -0,0 +1,12 @@
+package com.itemis.test;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesTaxesApplication {
+
+	public static void main(String[] args) {
+		System.out.println("ItemIs Project Structure");
+	}
+
+}
---
+++ src/test/java/com/itemis/test/TestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.itemis.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.*;
+
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(true, true);
+	}
+
+}
+
